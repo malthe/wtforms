5ca8d6f3082f351609b4dae07953d70fb5e1ddbb
Malthe Borch <mborch@gmail.com>
2013-11-25 09:03:50 +0100

    Preparing release.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info >= (3, ):
 
 setup(
     name='WTForms',
-    version='1.0.2.3',
+    version='1.0.2.4',
     url='http://wtforms.simplecodes.com/',
     license='BSD',
     author='Thomas Johansson, James Crasta',
